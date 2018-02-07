ad029570fc6f1d56a3bbebdb33d016d6b0a440dd
RAJNIK RADADIYA <rajnik144@gmail.com>
2018-02-07 22:31:19 +0530

    Changes configuration

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Game
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.delivery_method = :smtp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
